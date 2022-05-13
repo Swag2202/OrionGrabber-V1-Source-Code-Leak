aaa64314279d042d5bea4d673ba288a8ea7b5675
TheTeamHackFR <104196752+Th3Te4mH4ckFR@users.noreply.github.com>
2022-05-13 10:24:49 +0200

    Add files via upload

---
+++ OrionGrabber/OrionGrabber/MainForm.Designer.cs
@@ -0,0 +1,376 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 11/05/2022
+ * Time: 09:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace OrionGrabber
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.checkBox7 = new System.Windows.Forms.CheckBox();
+			this.checkBox8 = new System.Windows.Forms.CheckBox();
+			this.checkBox9 = new System.Windows.Forms.CheckBox();
+			this.checkBox10 = new System.Windows.Forms.CheckBox();
+			this.checkBox11 = new System.Windows.Forms.CheckBox();
+			this.checkBox12 = new System.Windows.Forms.CheckBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.pictureBox5);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(136, 528);
+			this.panel1.TabIndex = 0;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(142, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(706, 522);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(767, 27);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(33, 29);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Click += new System.EventHandler(this.PictureBox3Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(728, 27);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(33, 29);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.Click += new System.EventHandler(this.PictureBox4Click);
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(189, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(132, 48);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Orion";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox1.Location = new System.Drawing.Point(359, 43);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(120, 32);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "Main";
+			this.checkBox1.UseVisualStyleBackColor = false;
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(500, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(185, 48);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "+ Addons";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(43)))));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(208, 161);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(459, 44);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.Text = "Webhook URL";
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(673, 161);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 44);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Test";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox2.Location = new System.Drawing.Point(208, 222);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(271, 40);
+			this.checkBox2.TabIndex = 9;
+			this.checkBox2.Text = "Hide Stealer";
+			this.checkBox2.UseVisualStyleBackColor = false;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox3.Location = new System.Drawing.Point(208, 268);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(271, 40);
+			this.checkBox3.TabIndex = 10;
+			this.checkBox3.Text = "Run On Startup";
+			this.checkBox3.UseVisualStyleBackColor = false;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox4.Location = new System.Drawing.Point(208, 314);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(271, 40);
+			this.checkBox4.TabIndex = 11;
+			this.checkBox4.Text = "Trace Token";
+			this.checkBox4.UseVisualStyleBackColor = false;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox5.Location = new System.Drawing.Point(208, 360);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(271, 40);
+			this.checkBox5.TabIndex = 12;
+			this.checkBox5.Text = "Disable Internet";
+			this.checkBox5.UseVisualStyleBackColor = false;
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox6.Location = new System.Drawing.Point(208, 406);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(271, 40);
+			this.checkBox6.TabIndex = 13;
+			this.checkBox6.Text = "Melt Stealer";
+			this.checkBox6.UseVisualStyleBackColor = false;
+			// 
+			// checkBox7
+			// 
+			this.checkBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox7.Location = new System.Drawing.Point(208, 452);
+			this.checkBox7.Name = "checkBox7";
+			this.checkBox7.Size = new System.Drawing.Size(271, 34);
+			this.checkBox7.TabIndex = 14;
+			this.checkBox7.Text = "Anti VM";
+			this.checkBox7.UseVisualStyleBackColor = false;
+			// 
+			// checkBox8
+			// 
+			this.checkBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox8.Location = new System.Drawing.Point(467, 225);
+			this.checkBox8.Name = "checkBox8";
+			this.checkBox8.Size = new System.Drawing.Size(271, 34);
+			this.checkBox8.TabIndex = 15;
+			this.checkBox8.Text = "WebCam Picture";
+			this.checkBox8.UseVisualStyleBackColor = false;
+			// 
+			// checkBox9
+			// 
+			this.checkBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox9.Location = new System.Drawing.Point(467, 271);
+			this.checkBox9.Name = "checkBox9";
+			this.checkBox9.Size = new System.Drawing.Size(271, 34);
+			this.checkBox9.TabIndex = 16;
+			this.checkBox9.Text = "Take PC Info";
+			this.checkBox9.UseVisualStyleBackColor = false;
+			// 
+			// checkBox10
+			// 
+			this.checkBox10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox10.Location = new System.Drawing.Point(467, 320);
+			this.checkBox10.Name = "checkBox10";
+			this.checkBox10.Size = new System.Drawing.Size(333, 34);
+			this.checkBox10.TabIndex = 17;
+			this.checkBox10.Text = "Take Wifi Passwords";
+			this.checkBox10.UseVisualStyleBackColor = false;
+			// 
+			// checkBox11
+			// 
+			this.checkBox11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox11.Location = new System.Drawing.Point(467, 359);
+			this.checkBox11.Name = "checkBox11";
+			this.checkBox11.Size = new System.Drawing.Size(333, 40);
+			this.checkBox11.TabIndex = 18;
+			this.checkBox11.Text = "Anti Task Manager";
+			this.checkBox11.UseVisualStyleBackColor = false;
+			// 
+			// checkBox12
+			// 
+			this.checkBox12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+			this.checkBox12.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+			this.checkBox12.Location = new System.Drawing.Point(467, 404);
+			this.checkBox12.Name = "checkBox12";
+			this.checkBox12.Size = new System.Drawing.Size(333, 42);
+			this.checkBox12.TabIndex = 19;
+			this.checkBox12.Text = "Copy To Temp";
+			this.checkBox12.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(634, 452);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(178, 45);
+			this.button2.TabIndex = 20;
+			this.button2.Text = "Compile";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(136, 579);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox5.TabIndex = 1;
+			this.pictureBox5.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+			this.ClientSize = new System.Drawing.Size(851, 528);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.checkBox12);
+			this.Controls.Add(this.checkBox11);
+			this.Controls.Add(this.checkBox10);
+			this.Controls.Add(this.checkBox9);
+			this.Controls.Add(this.checkBox8);
+			this.Controls.Add(this.checkBox7);
+			this.Controls.Add(this.checkBox6);
+			this.Controls.Add(this.checkBox5);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.panel1);
+			this.ForeColor = System.Drawing.Color.White;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "OrionGrabber";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.CheckBox checkBox12;
+		private System.Windows.Forms.CheckBox checkBox11;
+		private System.Windows.Forms.CheckBox checkBox10;
+		private System.Windows.Forms.CheckBox checkBox9;
+		private System.Windows.Forms.CheckBox checkBox8;
+		private System.Windows.Forms.CheckBox checkBox7;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ OrionGrabber/OrionGrabber/MainForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 11/05/2022
+ * Time: 09:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OrionGrabber
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			MessageBox.Show("Welcome to Orion Grabber V0.1!");
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void PictureBox3Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		
+		void PictureBox4Click(object sender, EventArgs e)
+		{
+			WindowState = FormWindowState.Minimized;
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Compiled !");
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
